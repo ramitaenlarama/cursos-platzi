20a2225c8d0560d8e70b4dd113b1dd5ecccf8b4f
Cristian Ramanzin <ramanzincristian@gmail.com>
2021-09-17 12:37:48 -0300

    Personalizando la lista de productos

--- woocommerce/content-product.php
+++ woocommerce/content-product.php
@@ -25,43 +25,21 @@ if ( empty( $product ) || ! $product->is_visible() ) {
 }
 ?>
 <li <?php wc_product_class( '', $product ); ?>>
-	<?php
-	/**
-	 * Hook: woocommerce_before_shop_loop_item.
-	 *
-	 * @hooked woocommerce_template_loop_product_link_open - 10
-	 */
-	do_action( 'woocommerce_before_shop_loop_item' );
-
-	/**
-	 * Hook: woocommerce_before_shop_loop_item_title.
-	 *
-	 * @hooked woocommerce_show_product_loop_sale_flash - 10
-	 * @hooked woocommerce_template_loop_product_thumbnail - 10
-	 */
-	do_action( 'woocommerce_before_shop_loop_item_title' );
-
-	/**
-	 * Hook: woocommerce_shop_loop_item_title.
-	 *
-	 * @hooked woocommerce_template_loop_product_title - 10
-	 */
-	do_action( 'woocommerce_shop_loop_item_title' );
-
-	/**
-	 * Hook: woocommerce_after_shop_loop_item_title.
-	 *
-	 * @hooked woocommerce_template_loop_rating - 5
-	 * @hooked woocommerce_template_loop_price - 10
-	 */
-	do_action( 'woocommerce_after_shop_loop_item_title' );
-
-	/**
-	 * Hook: woocommerce_after_shop_loop_item.
-	 *
-	 * @hooked woocommerce_template_loop_product_link_close - 5
-	 * @hooked woocommerce_template_loop_add_to_cart - 10
-	 */
-	do_action( 'woocommerce_after_shop_loop_item' );
-	?>
+	<div class="productos__card">
+	<?php do_action( 'woocommerce_before_shop_loop_item_title' );?> 
+		<div class="producto__caption">
+			<?php do_action( 'woocommerce_before_shop_loop_item' ); ?>
+			<div class="productos__desc">
+				<p class="productos__precio">
+					<?php do_action( 'woocommerce_after_shop_loop_item_title' ); ?>
+				</p>
+				<h4 class="productos__titulo">
+					<?php do_action( 'woocommerce_shop_loop_item_title' ); ?>
+				</h4>
+			</div>
+			<div class="productos__cart">
+				<?php do_action( 'woocommerce_after_shop_loop_item' ); ?>
+			</div>
+		</div>
+	</div>
 </li>
