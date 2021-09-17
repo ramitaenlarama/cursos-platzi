e6883481028dbef65fd731696e4e3bde24a7ee2d
Cristian Ramanzin <ramanzincristian@gmial.com>
2021-09-17 12:12:30 -0300

    Convirtiendo nuestro theme en child theme de StoreFront

--- functions.php
+++ functions.php
@@ -7,9 +7,9 @@ function plz_assets(){
     wp_register_style("google-font-2","https://fonts.googleapis.com/css2?family=Roboto:wght@500&display=swap",array(),false,'all');
     wp_register_style("bootstrap","https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css",array(),"5-1",'all');
 
-    wp_enqueue_style("estilos", get_template_directory_uri()."/assets/css/style.css", array("google-font","bootstrap"));
+    wp_enqueue_style("estilos", get_stylesheet_directory_uri()."/assets/css/style.css", array("google-font","bootstrap"));
 
-    wp_enqueue_script("yardsale-js",get_template_directory_uri()."/assets/js/script.js");
+    wp_enqueue_script("yardsale-js",get_stylesheet_directory_uri()."/assets/js/script.js");
 }
 
 add_action("wp_enqueue_scripts","plz_assets");
