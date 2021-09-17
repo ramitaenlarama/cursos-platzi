6a725220cbb388fc2bb1f832a56de381bd1ba255
Cristian Ramanzin <ramanzincristian@gmial.com>
2021-09-17 12:22:13 -0300

    Aprovechando las funcionalidades de StoreFront

--- template-parts/content-header.php
+++ template-parts/content-header.php
@@ -3,7 +3,7 @@
         <div class="row">
             <div class="encabezado__hamburguesa col-2">
                 <a href="#">
-                    <img src="<?php echo get_template_directory_uri()?>/assets/img/icon-menu.svg" alt="menu icon">
+                    <img src="<?php echo get_stylesheet_directory_uri()?>/assets/img/icon-menu.svg" alt="menu icon">
                 </a>
             </div>
             <div class="enbezado__logo col-5 col-md-2 px-2 px-md-4">
@@ -21,7 +21,7 @@
                     <?php do_action("plz_signin"); ?>
                 </a>
                 <a href="#" class="encabezado__link active">
-                    <img src="<?php echo get_template_directory_uri();?>/assets/img/header-empty-card.svg" alt="cart">
+                    <img src="<?php echo get_stylesheet_directory_uri();?>/assets/img/header-empty-card.svg" alt="cart">
                 </a>
             </div>
         </div>
